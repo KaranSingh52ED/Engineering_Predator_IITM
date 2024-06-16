9153a0e824ba6baff9e5cff25bb3ef538be6cd8b
KaranSingh52ED <ed22b052@smail.iitm.ac.in>
2024-06-16 09:26:55 +0530

    initialization

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-red-400 text-2xl font-serif bg-green-300 text-center'>
+      My App
+    </div>
+  )
+}
+
+export default App
